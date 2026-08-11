ef3175d24e905bf37960e170d594c5f2b4e3a09d
Juan-Armoa <ilowsoul7@gmail.com>
2026-08-11 13:00:07 -0300

    feat: complete contact book with JSON load and save

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import json
 
-
-
 class Contact:
     def __init__(self, name, phone, email, country):
         self.name = name
@@ -24,20 +22,38 @@ class ContactBook:
     def add_contact(self, contact):
         self.contacts.append(contact)
         print("Contact added to the book!")
-        
+
     def save_contacts(self):
-        contacts_dict_list = [contact.to_dict() for contact in  self.contacts]
-        
+        contacts_dict_list = [contact.to_dict() for contact in self.contacts]
         with open("contacts.json", "w") as file:
             json.dump(contacts_dict_list, file, indent=4)
-            print("contacts saved to file succesfully!")
+            print("Contacts saved to file successfully!")
 
+    def load_contacts(self):
+        try:
+            with open("contacts.json", "r") as file:
+                contacts_data = json.load(file)
+                self.contacts = [
+                    Contact(c["name"], c["phone"], c["email"], c["country"])
+                    for c in contacts_data
+                ]
+            print("Contacts loaded successfully!")
+        except FileNotFoundError:
+            print("No contacts file found")
+
+# Pruebas
 p1 = Contact("Gonzalo", "11223344", "gonza@gmail.com", "Argentina")
 p2 = Contact("Aron", "11224214", "aron@gmail.com", "Argentina")
 
 my_book = ContactBook()
 my_book.add_contact(p1)
 my_book.add_contact(p2)
-
-print("Total contacts in book:", len(my_book.contacts))
 my_book.save_contacts()
+
+
+another_book = ContactBook()
+another_book.load_contacts()
+
+
+for c in another_book.contacts:
+    print(f"Name: {c.name} | Phone: {c.phone}")
